b33d8b78cf5a518dfc8c84847a935364411340df
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-05-27 16:54:46 +0300

    init

--- ArtWork/ContentView.swift
+++ ArtWork/ContentView.swift
@@ -9,8 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var gap = false
+    @State private var rotate = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(red: 0, green: 0, blue: 0)
+                .edgesIgnoringSafeArea(.all)
+
+            Text("Create ArtWork")
+                .font(.largeTitle)
+                .offset(y: -300)
+                .foregroundColor(.white)
+            
+            ZStack {
+                ForEach(0..<8) { item in
+                    Image(systemName: "paperclip")
+                        .resizable()
+                        .frame(width: self.rotate ? 220 : 180, height: self.rotate ? 220 : 180)
+                        .foregroundColor(.blue)
+                        .opacity(self.rotate ? 0.6 : 0.8)
+                        .hueRotation(.degrees(self.rotate ? 34 : 9))
+                        .offset(x: self.gap ? 0 : 95)
+                        .rotationEffect(.degrees(Double(item) * 45))
+                        .blendMode(self.rotate ? .lighten : .hardLight)
+                        .shadow(color: .blue, radius: 20, x: 0, y: 0)
+                }.rotationEffect(.degrees(self.rotate ? 360 : 0))
+            }
+        }.onTapGesture {
+            withAnimation {
+                self.rotate.toggle()
+                self.gap.toggle()
+            }
+        }
     }
 }
 
